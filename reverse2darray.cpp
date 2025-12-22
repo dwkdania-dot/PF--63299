8ef8928171a338fd60f74e125ac0184a441ef740
dwkdania-dot <dwk.dania@gmail.com>
2025-12-22 15:29:35 +0500

    Create reverse2darray.cpp

---
+++ reverse2darray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int row, col;
+    
+    cout << "Enter the number of rows: ";
+    cin >> row;
+    cout << "Enter the number of columns: ";
+    cin >> col;
+    
+    int arr[row][col];
+    
+    cout << "Enter all the elements in the matrix:" << endl;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << "Enter value for arr[" << i << "][" << j << "]: ";
+            cin >> arr[i][j];
+        }
+    }
+    
+    // Simple reverse: just print backwards!
+    cout << "Reversed 2D Array:" << endl;
+    for (int i = row - 1; i >= 0; i--) {
+        for (int j = col - 1; j >= 0; j--) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
